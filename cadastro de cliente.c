12e648d9c2f377ecb44346cd58e8d09441194316
fernandovales <vallesilva248@gmail.com>
2023-11-03 14:02:43 -0300

    menu de opcoes

--- cadastro de cliente.c
+++ cadastro de cliente.c
@@ -2,7 +2,33 @@
 #include<stdlib.h>
 #include<time.h>
 
+ struct infocliente{
+        char nome[100];
+        char email[100];
+};
+    
 int main(){
+    struct infocliente clientes[100];
+    int opcao,numclientes= 0;
+    
+    do {
+        printf("SELECIONE UMA OPÇÃO:\n");
+        printf("1- Adcionar cliente\n");
+        printf("2- Visualizar clientes\n");
+        printf("3- Editar cliente\n");
+        printf("4- Excluir cliente\n");
+        printf("5- Sair\n");
+        
+        scanf("%d",&opcao);
+        
+        switch(opcao){
+            
+                
+            
+        
+        } 
+    }while(opcao!=5);
+   
     
     return 0;
 }
